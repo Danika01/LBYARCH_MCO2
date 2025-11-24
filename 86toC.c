d9b37b4bec06f85167bb6164e4d473fdce49664f
Danika01 <168979440+Danika01@users.noreply.github.com>
2025-11-24 22:02:12 +0800

    Clean up 86toC.c code structure

--- 86toC.c
+++ 86toC.c
@@ -8,7 +8,8 @@ extern void compute(double *matrix, int *output, int rows);
 // automated random fill
 void fill(double *matrix, int rows)
 {
-    for (int i = 0; i < rows; i++) {
+    for(int i = 0; i < rows; i++)
+	{
         matrix[i*3 + 0] = (rand() % 200);       
         matrix[i*3 + 1] = (rand() % 200);     
         matrix[i*3 + 2] = (rand() % 10) + 1;   
@@ -28,10 +29,10 @@ void validate(double *matrix, int *output, int rows)
         double Vf = matrix[i*3 + 1] * 0.2777777778;
         double t  = matrix[i*3 + 2];
 
-        double acc = (Vf - Vi) / t;
-        int expected = (int)(acc + 0.5);
+        double acceleration = (Vf - Vi) / t;
+        int expected = (int)(acceleration + 0.5);
 
-        printf("Car %d → asm: %d | expected: %d\n", i, output[i], expected);
+        printf("Car %d -> asm: %d | expected: %d\n", i, output[i], expected);
     }
 }
 
@@ -52,6 +53,7 @@ double test(double *matrix, int *output, int rows)
 int main(){
 
     int mode;
+    
     printf("Select mode:\n");
     printf("1 = Manual input mode\n");
     printf("2 = Automatic performance test\n");
@@ -62,23 +64,19 @@ int main(){
     if(mode == 1)
 	{
         int num;
+        double *matrix = malloc(num * 3 * sizeof(double));
+        int *output = malloc(num * sizeof(int));
 
         printf("Enter number of cars: ");
         scanf("%d", &num);
 
-        double *matrix = malloc(num * 3 * sizeof(double));
-        int *output = malloc(num * sizeof(int));
-
         for(int i = 0; i < num; i++)
 		{
             printf("\nCar %d:\n", i + 1);
-
             printf(" Initial velocity (Vi): ");
             scanf("%lf", &matrix[i*3 + 0]);
-
             printf(" Final velocity (Vf): ");
             scanf("%lf", &matrix[i*3 + 1]);
-
             printf(" Time (t): ");
             scanf("%lf", &matrix[i*3 + 2]);
         }
@@ -107,22 +105,17 @@ int main(){
 
         for(int i = 0; i < count; i++)
 		{
-
             int rows = sizes[i];
-
-            printf("  Testing dataset size of %d rows\n", rows);
-
             double *matrix = malloc(rows * 3 * sizeof(double));
             int *output = malloc(rows * sizeof(int));
 
+            printf("Testing dataset size of %d rows\n", rows);
             fill(matrix, rows);
 
             double time = test(matrix, output, rows);
-
+            
             printf("Average time over 30 runs: %.8f seconds\n", time / 30.0);
-
             validate(matrix, output, rows);
-
             free(matrix);
             free(output);
         }
